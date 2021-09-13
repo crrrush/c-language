4900cd779d165a9eaf75d58f278c6699177570c1
crrrush <2293656642@qq.com>
2021-09-13 12:19:14 +0800

    GitHub text3

---
+++ Project2  8.13/Project2  8.13/test3.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+//加在源文件的第一行
+//define定义的标识符常量
+
+#include<stdio.h>
+
+
+int main()
+{
+
+
+
+	return 0;
+}
+
+//#define MAX 100
+//
+//
+//int main()
+//{
+//	int arr[MAX] = { 0 };
+//	printf("%d\n", MAX);
+//	int num1 = 1;
+//	int num2 = 1;
+//	int sum = 1;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+//const-常属性
+//const修饰常变量-是变量但又有常属性
+//枚举常量
+//枚举关键——enum
+//enum Sex{}
