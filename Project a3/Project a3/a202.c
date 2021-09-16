4ccc9255a6e18de6e38b14cb5b257aca76f5a683
crrrush <2293656642@qq.com>
2021-09-16 22:35:19 +0800

    second function

--- Project a3/Project a3/a202.c
+++ Project a3/Project a3/a202.c
@@ -3,27 +3,44 @@
 
 #include<stdio.h>
 
-int Y(int x)
-{
-	int z = 0;
-	int k = 0;
-	int b = 0;
-	scanf("%d%d", &k, &b);
-	z = k*x + b;
-	return z;
-}
+//int f(int x)
+//{
+//	int a, b, c, z;
+//	scanf("%d%d%d", &a, &b, &c);
+//	z= a * x*x + b * x + c;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int n, y;
+//	scanf("%d", &n);
+//	y = f(n);
+//	printf("y=%d\n", y);
+//	return 0;
+//}
 
-
-int main()
-{
-	int a = 0;
-	int y = 0;
-	scanf("%d", &a);
-	y = Y(a);
-	printf("y=%d\n", y);
-
-	return 0;
-}
+//int Y(int x)
+//{
+//	int z = 0;
+//	int k = 0;
+//	int b = 0;
+//	scanf("%d%d", &k, &b);
+//	z = k*x + b;
+//	return z;
+//}
+//
+//
+//int main()
+//{
+//	int a = 0;
+//	int y = 0;
+//	scanf("%d", &a);
+//	y = Y(a);
+//	printf("y=%d\n", y);
+//
+//	return 0;
+//}
 
 
 //int ADD(int x, int y)
