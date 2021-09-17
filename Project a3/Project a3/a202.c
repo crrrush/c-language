09003ac5e3b1ffdabbec7502faec01dc8be9d3a9
crrrush <2293656642@qq.com>
2021-09-17 18:36:01 +0800

    2function

---
+++ Project a3/Project a3/a202.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+
+//int f(int x)
+//{
+//	int a, b, c, z;
+//	scanf("%d%d%d", &a, &b, &c);
+//	z= a * x*x + b * x + c;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int n, y;
+//	scanf("%d", &n);
+//	y = f(n);
+//	printf("y=%d\n", y);
+//	return 0;
+//}
+
+//int Y(int x)
+//{
+//	int z = 0;
+//	int k = 0;
+//	int b = 0;
+//	scanf("%d%d", &k, &b);
+//	z = k*x + b;
+//	return z;
+//}
+//
+//
+//int main()
+//{
+//	int a = 0;
+//	int y = 0;
+//	scanf("%d", &a);
+//	y = Y(a);
+//	printf("y=%d\n", y);
+//
+//	return 0;
+//}
+
+
+//int ADD(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d%d", &a, &b);
+//	c = ADD(a, b);
+//	printf("c=%d\n", c);
+//	return 0;
+//}
