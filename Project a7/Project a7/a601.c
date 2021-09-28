e34f31751166685339afeb3697b07ef6e78556f2
crrrush <2293656642@qq.com>
2021-09-28 22:52:04 +0800

    累阶乘

---
+++ Project a7/Project a7/a601.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int m, n, a, b;
+//	int r, q;
+//	printf("输入两个数字：\n");
+//	scanf("%d%d", &m, &n);
+//	a = m, b = n;
+//	if (m >= n)
+//	{
+//		
+//		r = m % n;
+//		while (r > 0)
+//		{
+//			m = n;
+//			n = r;
+//			r = m % n;
+//
+//		}
+//		if (r <= 0)
+//		{
+//			printf("两数最大公约数是%d\n", n);
+//			q = (a*b) / n;
+//			printf("两数最小公倍数是%d\n", q);
+//
+//		}
+//	}
+//	else
+//	{
+//		
+//		r = n % m;
+//		while (r > 0)
+//		{
+//			n = m;
+//			m = r;
+//			r = n % m;
+//
+//		}
+//		if (r <= 0)
+//		{
+//			printf("两数最大公约数是%d\n", m);
+//			q = (a*b) / n;
+//			printf("两数最小公倍数是%d\n", q);
+//
+//		}
+//	}
+//		
+//	
+//
+//
+//	return 0;
+//}
---
+++ Project a7/Project a7/a602.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()   //阶乘自研
+//{
+//	int i=1, n;
+//	double x = 1, sum = 0;//最终版本
+//	scanf("%d", &n);
+//	while (i <= n)
+//	{
+//
+//		x *= i;
+//		sum = sum + x;
+//		i++;
+//	}	
+//	printf("%lf", sum);
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
+
+//int main()      //阶乘online
+//{
+//	int i, n;
+//	double sum = 1;
+//	scanf("%d", &n);
+//	for ( i = 1; i <= n; i++)
+//	{
+//		sum = sum * i;
+//	}
+//	printf("sum=%f", sum);
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+//int main()    //累加
+//{
+//	int n = 0;
+//	int i = 1, sum = 0;
+//	scanf("%d", &n);
+//	while (i<=n)
+//	{
+//		sum = sum + i;
+//		i++;
+//	}
+//	printf("%d\n", sum);
+//
+//
+//
+//
+//	return 0;
+//}
