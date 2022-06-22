b99cda304c4802fff74be4b95877a1cbd3c0378f
crrrush <2293656642@qq.com>
2022-06-22 18:06:07 +0800

    test 1

---
+++ 1-2/1-2/1-2.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//#define N 30
+//
+//int Calc(int* PA, int Num)
+//{
+//	int count = 0;
+//	while (Num--)
+//	{
+//		count += *PA;
+//		PA++;
+//	}
+//	return count;
+//}
+//
+//
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	printf("输入数组中元素的个数：\n");
+//	scanf("%d", &n);
+//
+//	int PArray[N] = { 0 };
+//
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &PArray[i]);
+//	}
+//	printf("output：\n");
+//	printf("数组的元素和是：%d\n", Calc(PArray, n));
+//
+//	return 0;
+}
+
