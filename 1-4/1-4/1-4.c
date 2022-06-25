fccdd33e09cefa6938c03a3670a8372b2da7fbc4
crrrush <2293656642@qq.com>
2022-06-25 18:29:24 +0800

    test4

---
+++ 1-4/1-4/1-4.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+
+
+//int main()
+//{
+//	int i = 0, j = 0;
+//
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < i + 5; j++)
+//		{
+//			if (j < 4 - i)
+//				printf(" ");
+//			else
+//				printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	for (i = 0; i < 4; i++)
+//	{
+//		for (j = 0; j < i + 5; j++)
+//		{
+//			if (j < 4 - i)
+//				printf(" ");
+//			else
+//				printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 0; j < i + 5; j++)
+//		{
+//			if (j < 4 - i)
+//				printf(" ");
+//			else
+//				printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 0; j < 6; j++)
+//		{
+//			if (j < 3)
+//				printf(" ");
+//			else
+//				printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	for (i = 0; i < 2; i++)
+//	{
+//		for (j = 0; j < 10; j++)
+//			printf("*");
+//
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
