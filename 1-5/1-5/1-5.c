83fbe894f300f7c007911d720dc3406665676a3a
crrrush <2293656642@qq.com>
2022-06-26 16:25:45 +0800

    test5

---
+++ 1-5/1-5/1-5.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 0, j = 0;
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 0; j < 2 * i + 1; j++)
+//		{
+//			printf("* ");
+//		}
+//		printf("\n");
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//		printf("* ");
+//
+//	printf("\n");
+//
+//	for (i = 4; i > 0; i--)
+//	{
+//		for (j = 0; j < i; j++)
+//			printf("* * ");
+//
+//		printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
